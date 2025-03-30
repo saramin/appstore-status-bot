85038e8e9f81337a17b1dd8600ba5c26d946d2e9
Dongmin Yu <ydm2790@gmail.com>
2025-03-30 13:09:12 +0900

    feat: ⚙️ add process environment shortcuts for appstore connect
    Adds environment variables for:
    
    - `PRIVATE_KEY`: API Key file (.p8)
    - `KEY_ID`: Appstore connect `key_id`
    - `ISSUER_ID`: Appstore connect `issuer_id`
    - `BUNDLE_ID`: Application bundle identifier(s)
    
    Also, sets the default `LANGUAGE` to `ko` if not provided in the environment.

--- Sources/env.js
+++ Sources/env.js
@@ -1,4 +1,20 @@
 /**
+ * Input raw data about your API Key file (.p8)  
+ */
+export const PRIVATE_KEY = process.env.PRIVATE_KEY;
+/**
+ * Input Appstore connect `key_id`  
+ */
+export const KEY_ID = process.env.KEY_ID;
+/**
+ * Input Appstore connect `issuer_id`  
+ */
+export const ISSUER_ID = process.env.ISSUER_ID;
+/**
+ * Input your bundle_identifier of application you can input multiple bundle_id with comma and no whitespace  
+ */
+export const BUNDLE_ID = process.env.BUNDLE_ID;
+/**
  * The Slack webhook URL to use for sending notifications.
  */
 export const SLACK_WEBHOOK = process.env.SLACK_WEBHOOK;
@@ -18,4 +34,4 @@ export const GIST_ID = process.env.GIST_ID;
  * The language to use if the `LANGUAGE` environment variable is not set.
  * @kind "en" | "ko" | "ja"
  */
-export const LANGUAGE = process.env.LANGUAGE;
+export const LANGUAGE = process.env.LANGUAGE ?? 'ko';
